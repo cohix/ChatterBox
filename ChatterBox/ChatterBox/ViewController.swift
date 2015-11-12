8060fd43455ccf6056ce718c49510e105500f29c
Connor Hicks <connor@agilebits.com>
2015-11-11 19:47:47 -0500

    Added test code

--- ChatterBox/ChatterBox/ViewController.swift
+++ ChatterBox/ChatterBox/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureAudioDataOutputSampleBufferDelegate
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupMicrophone()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupMicrophone()
+    {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSessionPresetMedium
+        
+        let mic = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeAudio)
+        var mic_input: AVCaptureDeviceInput!
+        
+        let audio_output = AVCaptureAudioDataOutput()
+        audio_output.setSampleBufferDelegate(self, queue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+        
+        do
+        {
+            mic_input = try AVCaptureDeviceInput(device: mic)
+        }
+        catch
+        {
+            return
+        }
+        
+        session.addInput(mic_input)
+        session.addOutput(audio_output)
+        
+        session.startRunning()
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!)
+    {
+        print("hello")
+    }
 }
 
