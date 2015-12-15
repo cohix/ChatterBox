4c1d2a89bf080d8340620fe1ffc5b8bdbb5a4937
cohix <connor@spottedu.co>
2015-12-15 15:49:55 -0500

    Audio working

--- ChatterBox/ChatterBox/CBNetworkManager.swift
+++ ChatterBox/ChatterBox/CBNetworkManager.swift
@@ -9,47 +9,49 @@
 import Foundation
 import AVFoundation
 import Socket_IO_Client_Swift
+import CocoaAsyncSocket
 
 class CBNetworkManager : NSObject{
     
-    var socket: SocketIOClient!
+    var messageSocket: SocketIOClient!
+    var streamingSocket: GCDAsyncSocket!
     
     override init(){
         super.init()
         self.setup()
         self.addHandlers()
-        self.socket.connect()
+        self.messageSocket.connect()
     }
     
     func setup(){
-        self.socket = SocketIOClient(socketURL: "localhost:8900")
+        self.messageSocket = SocketIOClient(socketURL: "localhost:8900")
     }
     
     func addHandlers(){
-        self.socket.onAny {
+        self.messageSocket.onAny {
             print("got event: \($0.event) with items \($0.items)")
         }
-        self.socket.on("&roomupdate") {[weak self] data, ack in
+        self.messageSocket.on("&roomupdate") {[weak self] data, ack in
             
             return
         }
     }
     
     func createUser(user: User){
-      self.socket.emit("&create;<" + user.getName() + ">&endm;<" + user.getPassword() + ";>endm;")
+      self.messageSocket.emit("&create;<" + user.getName() + ">&endm;<" + user.getPassword() + ";>endm;")
     }
     
     func login(user : User){
-        self.socket.emit("&create;<" + user.getName() + ">&endm;<" + user.getPassword() + ";>endm;")
+        self.messageSocket.emit("&create;<" + user.getName() + ">&endm;<" + user.getPassword() + ";>endm;")
     }
     
     // not sure how we're approaching this. should we have the chat room classes? seems like kyle would access them from the db but maybe I'm wrong? do we have direct access to the db too?
     func joinRoom(){
-        self.socket.emit("&joinroom;<" + ">&endm;")
+        self.messageSocket.emit("&joinroom;<" + ">&endm;")
     }
     
     func privateCall(user: User){
-        self.socket.emit("&private;<" + user.username + ">&endm;")
+        self.messageSocket.emit("&private;<" + user.username + ">&endm;")
     }
 }
 
